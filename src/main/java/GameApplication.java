70cc8e3cc4729c4e33df5d8bf432ddcd55db05c7
keepbang <rlqud1125@naver.com>
2022-02-22 10:28:44 +0900

    feat: Add main method

--- src/main/java/GameApplication.java
+++ src/main/java/GameApplication.java
@@ -1,16 +1,4 @@
 public class GameApplication {
-    public static void main(String[] args) {
-        // 종족 선택 (UI)
-        // 캐릭터 새성
-        // 무기 선택 (UI)
-        // 몬스터 생성
-        // 전투 시작
-        // 공격과 스킬중 하나만 선택 (UI)
-            // 공격일경우
-                // 캐릭터가 몬스터를 공격
-                // 몬스터 반격 여부에 따라 캐릭터 HP 감소
-                // 공격 속도만큼 delay 발생
-            // 스킬 선택
-                // 캐릭터가 스킬 사용
+    public static void main(String[] args) throws InterruptedException {
     }
 }
